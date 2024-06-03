b2b844a729bc828620c98eebe79656cff3137973
余空 <70465933+YuKongA@users.noreply.github.com>
2024-06-03 21:51:42 +0800

    app: fix night theme

--- app/src/main/kotlin/top/yukonga/hq_icon/ui/theme/Theme.kt
+++ app/src/main/kotlin/top/yukonga/hq_icon/ui/theme/Theme.kt
@@ -7,6 +7,18 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
+private val LightColorScheme = lightColorScheme(
+    primary = Color(0xFF3482FF),
+    primaryContainer = Color(0xFFF7F7F7),
+    onPrimaryContainer = Color.Black,
+    background = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+)
+
 private val DarkColorScheme = darkColorScheme(
     primary = Color(0xFF277AF7),
     primaryContainer = Color(0xFF212121),
@@ -17,18 +29,6 @@ private val DarkColorScheme = darkColorScheme(
     onTertiary = Color.Black,
     onBackground = Color.White,
     onSurface = Color.White,
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Color(0xFF0D84FF),
-    primaryContainer = Color(0xFFF5F5F5),
-    onPrimaryContainer = Color.Black,
-    background = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
 )
 
 @Composable
